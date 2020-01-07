98cb583977a3fac6a534b383afba88aebeab22b7
Zach Fox <zafax947@gmail.com>
2020-01-07 18:17:08 -0500

    Removed Old Commands
    Removed the testing commands such as ping and pong

--- index.js
+++ index.js
@@ -20,22 +20,6 @@ bot.on("ready", () => {
 
 bot.registerCommandAlias("halp", "help"); // Alias !halp to !help
 
-bot.registerCommand("ping", "Pong!", {
-  // Make a ping command
-  // Responds with "Pong!" when someone says "!ping"
-  description: "Pong!",
-  fullDescription:
-    "This command could be used to check if the bot is up. Or entertainment when you're bored."
-});
-
-bot.registerCommand("pong", ["Pang!", "Peng!", "Ping!", "Pung!"], {
-  // Make a pong command
-  // Responds with a random version of "Ping!" when someone says "!pong"
-  description: "Ping!",
-  fullDescription:
-    "This command could also be used to check if the bot is up. Or entertainment when you're bored."
-});
-
 bot.registerCommand(
   "score",
   (msg, args) => {
